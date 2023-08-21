42e8b470b823239173344213d1a017ee86e9e200
devjoppe <joakim.ottosson@medieinstitutet.se>
2023-08-21 08:40:23 +0200

    Adding card to the list item

--- src/components/list/ListItem.tsx
+++ src/components/list/ListItem.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import { Card, CardHeader, CardBody, CardFooter, Flex, Box, Heading, Text, IconButton} from '@chakra-ui/react'
+import { BsFillBookmarkStarFill } from "react-icons/bs";
 import { hnItemInt } from "../../interfaces/hnInterfaces.interface"
 
 interface Iprop {
@@ -7,9 +9,24 @@ interface Iprop {
 
 const ListItem:React.FC<Iprop> = ({itemData}) => {
     return(
-        <div key={itemData.title}>
-            {itemData.title}
-        </div>
+        <Card mb="6">
+            <CardHeader>
+                <Flex>
+                    <Box flex='1'>
+                        <Heading mb="2" size='sm'>{itemData.title}</Heading>
+                    </Box>
+                    <IconButton
+                        variant='ghost'
+                        colorScheme='gray'
+                        aria-label='See menu'
+                        icon={<BsFillBookmarkStarFill />}
+                    />
+                </Flex>
+            </CardHeader>
+            <CardBody>
+                <Text fontSize='sm'>{itemData.points} points by {itemData.author} with {itemData.num_comments} comments</Text>
+            </CardBody>
+        </Card>
     )
 }
 
